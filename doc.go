8f6a652cacf017d2f34b0147a01124fcd3e10116
ramory-l <ramory.lorch@icloud.com>
2026-05-16 00:28:39 +0300

    chore: scaffold easydi module

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package easydi is a reflection-free, compile-time dependency-injection
+// code generator driven by // di: comment annotations.
+package easydi
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ramory-l/easydi
+
+go 1.26
